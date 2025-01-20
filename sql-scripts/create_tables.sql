c7cd6f24e62c8d26f529fce3a4fea1fba51ac8bc
Rohan Kumar <31317534+rohanksingh@users.noreply.github.com>
2025-01-20 07:23:48 -0500

    Update create_tables.sql

--- sql-scripts/create_tables.sql
+++ sql-scripts/create_tables.sql
@@ -1,19 +1,65 @@
-CREATE TABLE IF NOT EXISTS customer (
-    customer_id INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
+use sales_db;
+
+
+--  CI/CD pipeline example for MySQL using the earlier orders, customer, and region_sales_summary tables.
+
+-- STEP 1 : sql Scripts
+
+create table orders (
+order_id int primary key,
+customer_id int,
+order_date Date,
+amount decimal(10,2)
 );
 
-CREATE TABLE IF NOT EXISTS orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    amount DECIMAL(10, 2),
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+create table customer (
+customer_id int primary key ,
+customer_name varchar(100),
+region varchar(50)
 );
 
-CREATE TABLE IF NOT EXISTS region_sales_summary (
-    region VARCHAR(50),
-    total_sales DECIMAL(10, 2)
+insert into orders values (1, 101, '2023-12-01', 150.00),
+						(2, 102, '2023-12-02' , 200.00),
+                        (3, 103, '2023-12-05' , 300.00),
+						(4, 104, '2023-12-07' , 210.00),
+                        (5, 105, '2023-12-08' , 310.00),
+						(6, 106, '2023-12-09' , 400.00),
+						(7, 107, '2023-12-11' , 410.00),
+                        (8, 108, '2023-12-13' , 510.00);
+                        
+                        
+insert into customer values (101, 'Alice', 'North'),
+							(102, 'Bob' , 'South'),
+                            (103, 'Rohan', 'East'),
+							(104, 'Singh' , 'West'),
+							(105, 'Sob' , 'South'),
+                            (106, 'ber', 'East'),
+							(107, 'roz' , 'West'),
+							(108, 'ros' , 'North');
+                            
+create table region_sales (
+region varchar(50),
+total_sales decimal (10, 2)
 );
+
+insert into region_sales (region, total_sales)
+select 
+c.region,
+sum(o.amount) as total_sales
+from 
+orders o 
+join customer c on o.customer_id = c.customer_id
+where 
+c.region in ('North' , 'South')
+group by 
+c.region;
+
+
+-- Create target table 
+
+create table region_sales_summary (
+region varchar(50),
+total_sales decimal (10, 2)
+);
+
 
